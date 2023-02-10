31baeafe0e785eaf9d45f95e133edb44475a3a34
LFF-Server <meerschweinchen5644@gmail.com>
2023-02-10 20:43:44 +0100

    add arg exampel

---
+++ server/getInput.js
@@ -0,0 +1,2 @@
+response.write("Benutze '?test=1&lol&boolean&str=hallo&number=346' nach der url und hier kannst du dir dann deine angaben ansehen das ist in 2 Zeilen code möglich dank dem guten system xD\n\n");
+response.write(JSON.stringify(input,null,2));
