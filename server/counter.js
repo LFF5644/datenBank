eafb41bd5aaedb044af57a521daa73f74a7f8f05
LFF-Server <meerschweinchen5644@gmail.com>
2023-02-11 10:24:59 +0100

    better counter

--- server/counter.js
+++ server/counter.js
@@ -1,6 +1,32 @@
+const fs=require("fs");
+const counterFile="counter.save";
+const {
+	load,
+	save,
+	reset,
+}=input;
+
+
 if(globals.counter){
 	globals.counter+=1;
 }else{
 	globals.counter=1;
 }
-response.write("COUNTER: "+globals.counter);
+if(reset){
+	response.write("counter reset!\n")
+	globals.counter=1;
+}
+if(save){
+	fs.writeFileSync(counterFile,String(globals.counter),"utf-8");
+	response.write("counter saved to file!\n")
+}
+else if(load){
+	try{
+		globals.counter=Number(fs.readFileSync(counterFile,"utf-8"));
+		response.write("counter loaded from file!\n")
+	}
+	catch(e){
+		response.write("cant read file!\n")
+	}
+}
+response.write("\nCOUNTER: "+globals.counter+"\n");
